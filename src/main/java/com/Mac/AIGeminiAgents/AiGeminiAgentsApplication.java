e7bc06c0d997122252821e84111bb02651cab3e9
Maciej Jan Szczygieł <maciooox3@gmail.com>
2025-10-27 12:22:08 +0100

    init
    init

---
+++ src/main/java/com/Mac/AIGeminiAgents/AiGeminiAgentsApplication.java
@@ -0,0 +1,13 @@
+package com.Mac.AIGeminiAgents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGeminiAgentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiGeminiAgentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Mac/AIGeminiAgents/AiGeminiAgentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mac.AIGeminiAgents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGeminiAgentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
